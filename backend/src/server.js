2724ed970605ea907b02bcf890afd2594b627f3a
Udit Singh <uditsingh8642@gmail.com>
2026-04-28 14:48:17 +0530

    fix: remove static frontend serving from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
@@ -9,7 +8,7 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
-const __dirname = path.resolve();
+
 
 const PORT = ENV.PORT || 3000;
 
@@ -25,14 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
