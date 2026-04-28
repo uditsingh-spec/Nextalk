d5322b706e851c16bb828e9422c538cf2e8b0c47
Udit Singh <uditsingh8642@gmail.com>
2026-04-28 15:01:11 +0530

    fix: update cookie sameSite for cross-domain production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,9 +12,9 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: ENV.NODE_ENV === "development" ? "strict" : "none",
     secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
