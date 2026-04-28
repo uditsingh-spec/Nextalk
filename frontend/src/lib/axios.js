41b1be2bc2de8db18372b25e561131b9f7748312
Udit Singh <uditsingh8642@gmail.com>
2026-04-28 14:40:55 +0530

    fix: update CORS for production deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" :  "https://nextalk-backend-xges.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development"
+   ? "http://localhost:3000/api" 
+   :  "https://nextalk-backend-xges.onrender.com/api",
   withCredentials: true,
 });
